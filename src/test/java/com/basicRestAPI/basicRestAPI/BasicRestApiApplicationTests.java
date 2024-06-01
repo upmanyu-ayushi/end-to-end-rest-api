3b1713faa9b15073b3f366267605ad2cc515dd69
Ayushi Upmanyu <ayushi.upmanyu@bloomreach.com>
2024-06-01 08:28:15 +0530

    Initial commit

---
+++ src/test/java/com/basicRestAPI/basicRestAPI/BasicRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicRestAPI.basicRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
